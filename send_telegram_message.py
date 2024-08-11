abf6f82e823d547c381941bdce7c3a2d38c12532
jojolash <167390089+jojolash@users.noreply.github.com>
2024-08-11 22:49:26 +0800

    Update send_telegram_message.py

--- send_telegram_message.py
+++ send_telegram_message.py
@@ -1,29 +1,19 @@
 import requests
 import os
 
-# 从环境变量中获取 Telegram Bot Token 和 Chat ID
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
 def send_telegram_message(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f'https://api.telegram.org/bot{os.getenv("TELEGRAM_BOT_TOKEN")}/sendMessage'
     payload = {
-        'chat_id': TELEGRAM_CHAT_ID,
+        'chat_id': os.getenv('TELEGRAM_CHAT_ID'),
         'text': message,
-        'parse_mode': 'Markdown'  # 可选的消息格式化模式
+        'parse_mode': 'Markdown'
     }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    try:
-        response = requests.post(url, json=payload, headers=headers)
-        if response.status_code == 200:
-            print("消息成功发送到 Telegram")
-        else:
-            print(f"发送消息到 Telegram 失败: {response.text}")
-    except Exception as e:
-        print(f"发送消息到 Telegram 时出错: {e}")
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code != 200:
+        raise ValueError(f'Error: {response.status_code} {response.text}')
 
 if __name__ == '__main__':
-    message = "消息示例"
+    with open('ssh_output.txt', 'r') as file:
+        message = file.read()
     send_telegram_message(message)
